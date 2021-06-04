a48e3f4be51af1754795eeb715a3d495aa57c850
Fabrizio <fabry_98@hotmail.it>
2021-06-04 17:51:02 +0200

    Added rooms support

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ let pingStartTime = null;
 
 const httpsServer = require("https").createServer(
 	{
-		key: fs.readFileSync("certs/private.key"),
-		cert: fs.readFileSync("certs/certificate.crt"),
+		key: fs.readFileSync("certs/key.pem"),
+		cert: fs.readFileSync("certs/cert.pem"),
 	},
 	(req, res) => {
 		fs.readFile(__dirname + req.url, (err, data) => {
@@ -23,27 +23,46 @@ const httpsServer = require("https").createServer(
 
 const io = require("socket.io")(httpsServer);
 
-io.on("connection", (socket) => {
-	console.log("User connected", socket.id);
+function getRoomId(socket) {
+	for (const roomId of socket.rooms) if (roomId != null && roomId != socket.id) return roomId;
+}
 
+io.on("connection", (socket) => {
 	socket.on("note_on", (data) => {
-		console.log("Received note on", data);
-		socket.broadcast.emit("note_on", data);
+		const roomId = getRoomId(socket);
+		socket.to(roomId).emit("note_on", data);
 	});
 
 	socket.on("note_off", (data) => {
-		console.log("Received note off", data);
-		socket.broadcast.emit("note_off", data);
+		const roomId = getRoomId(socket);
+		socket.to(roomId).emit("note_off", data);
 	});
 
+	socket.on("disconnecting", () => {
+		socket.roomLeft = getRoomId(socket);
+	});
 	socket.on("disconnect", (reason) => {
-		console.log("Client disconnected");
+		io.to(socket.roomLeft).emit("userDisconnected", socket.username);
 	});
 
 	socket.on("pong", () => {
 		const now = Date.now();
 		const latency = now - pingStartTime;
-		socket.emit("latency", latency);
+		const roomId = getRoomId(socket);
+		io.to(roomId).emit("latency", { username: socket.username, latency });
+	});
+
+	socket.on("joinRoom", (data) => {
+		const { roomId, username } = data;
+		socket.username = username;
+		socket.join(roomId);
+
+		io.to(roomId).emit("newUser", socket.username);
+		const roomClients = io.of("/").adapter.rooms.get(roomId);
+		for (const socketId of roomClients) {
+			const socket = io.of("/").sockets.get(socketId);
+			io.to(socket).emit("newUser", socket.username);
+		}
 	});
 });
 
