7b46d8308fbf6bb9a8cba048d5cbb645e5fbb260
Matteo <orsini.matteo99@gmail.com>
2021-06-05 11:05:06 +0200

    Add username to note_on and note_off events

--- server.js
+++ server.js
@@ -6,78 +6,79 @@ const LATENCY_PERIOD_MS = 2000;
 let pingStartTime = null;
 
 const httpsServer = require("https").createServer(
-	{
-		key: fs.readFileSync("certs/key.pem"),
-		cert: fs.readFileSync("certs/cert.pem"),
-	},
-	(req, res) => {
-		fs.readFile(__dirname + req.url, (err, data) => {
-			if (err) {
-				res.writeHead(404);
-				res.end(JSON.stringify(err));
-				return;
-			}
-			res.writeHead(200);
-			res.end(data);
-		});
-	}
+  {
+    key: fs.readFileSync("certs/key.pem"),
+    cert: fs.readFileSync("certs/cert.pem"),
+  },
+  (req, res) => {
+    fs.readFile(__dirname + req.url, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
 );
 
 const io = require("socket.io")(httpsServer);
 if (process.env.REDIS_HOST) {
-	const redis = require("socket.io-redis");
-	io.adapter(redis({ host: process.env.REDIS_HOST, port: 6379 }));
+  const redis = require("socket.io-redis");
+  io.adapter(redis({ host: process.env.REDIS_HOST, port: 6379 }));
 }
 
 function getRoomId(socket) {
-	for (const roomId of socket.rooms) if (roomId != null && roomId != socket.id) return roomId;
+  for (const roomId of socket.rooms)
+    if (roomId != null && roomId != socket.id) return roomId;
 }
 
 io.on("connection", (socket) => {
-	socket.on("note_on", (data) => {
-		const roomId = getRoomId(socket);
-		socket.to(roomId).emit("note_on", data);
-	});
+  socket.on("note_on", (data) => {
+    const roomId = getRoomId(socket);
+    socket.to(roomId).emit("note_on", { ...data, username: socket.username });
+  });
 
-	socket.on("note_off", (data) => {
-		const roomId = getRoomId(socket);
-		socket.to(roomId).emit("note_off", data);
-	});
+  socket.on("note_off", (data) => {
+    const roomId = getRoomId(socket);
+    socket.to(roomId).emit("note_off", { ...data, username: socket.username });
+  });
 
-	socket.on("disconnecting", () => {
-		socket.roomLeft = getRoomId(socket);
-	});
-	socket.on("disconnect", (reason) => {
-		io.to(socket.roomLeft).emit("userDisconnected", socket.username);
-	});
+  socket.on("disconnecting", () => {
+    socket.roomLeft = getRoomId(socket);
+  });
+  socket.on("disconnect", (reason) => {
+    io.to(socket.roomLeft).emit("userDisconnected", socket.username);
+  });
 
-	socket.on("pong", () => {
-		const now = Date.now();
-		const latency = now - pingStartTime;
-		const roomId = getRoomId(socket);
-		io.to(roomId).emit("latency", { username: socket.username, latency });
-	});
+  socket.on("pong", () => {
+    const now = Date.now();
+    const latency = now - pingStartTime;
+    const roomId = getRoomId(socket);
+    io.to(roomId).emit("latency", { username: socket.username, latency });
+  });
 
-	socket.on("joinRoom", (data) => {
-		const { roomId, username } = data;
-		socket.username = username;
-		socket.join(roomId);
+  socket.on("joinRoom", (data) => {
+    const { roomId, username } = data;
+    socket.username = username;
+    socket.join(roomId);
 
-		io.to(roomId).emit("newUser", socket.username);
-		const roomClients = io.of("/").adapter.rooms.get(roomId);
-		for (const socketId of roomClients) {
-			const socket = io.of("/").sockets.get(socketId);
-			io.to(socket.id).emit("newUser", socket.username);
-		}
-	});
+    io.to(roomId).emit("newUser", socket.username);
+    const roomClients = io.of("/").adapter.rooms.get(roomId);
+    for (const socketId of roomClients) {
+      const socket = io.of("/").sockets.get(socketId);
+      io.to(socket.id).emit("newUser", socket.username);
+    }
+  });
 });
 
 setInterval(() => {
-	pingStartTime = Date.now();
-	io.emit("ping");
+  pingStartTime = Date.now();
+  io.emit("ping");
 }, LATENCY_PERIOD_MS);
 
 const port = process.argv[2];
 httpsServer.listen(port, () => {
-	console.log("Server started on port", port);
+  console.log("Server started on port", port);
 });
